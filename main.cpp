4b4e3cf0588af64460121a74b3a233361cc06f68
Roberto Bampi <bampi.roberto@gmail.com>
2014-05-13 21:41:39 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+struct node_t {
+    vector<int> neighbors;
+};
+
+typedef vector<node_t> graph_t;
+
+vector<int> solve(const graph_t &graph){
+
+}
+
+int main(int argc, char ** argv){
+    ifstream in("input.txt");
+    graph_t graph;
+    int size,M;
+    in>>size>>M;
+    graph.resize(size);
+    for(int i=0;i<M;i++){
+        int from, to;
+        in >> from >> to;
+        graph[from].neighbors.push_back(to);
+        graph[to].neighbors.push_back(from);
+    }
+
+    vector<int> sol = solve(graph);
+    ofstream out("output.txt");
+    out << sol.size() << endl;
+    for(int i: sol){
+        out << i << endl;
+    }
+    return 0;
+}
