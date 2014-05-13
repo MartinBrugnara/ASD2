41ffb1e0456fc429409c8b5c69b2636cdc1a1da6
Roberto Bampi <bampi.roberto@gmail.com>
2014-05-13 21:45:29 +0200

    correction script

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+clang++ -O2 -std=c++11 -o DatasetP2/cor DatasetP2/cor.cpp
+
 # 19 not computable
 tests=18
 if [[ -n $1 ]]; then
@@ -11,10 +13,13 @@ clang++ -std=c++11 -O2 -o psy psy.cpp -DEVAL
 
 for i in $(eval echo {$range})
 do
-        cp DatasetP1/input/input$i.txt input.txt
+        cp DatasetP2/input/input$i.txt input.txt
         echo -n "$i: "
         (time ./generali) &> /tmp/res && cat /tmp/res | grep real 
         echo -n "   "
-        ./DatasetP1/cor DatasetP1/input/input$i.txt DatasetP1/helper/helper$i.txt output.txt
+        ./DatasetP2/cor DatasetP2/input/input$i.txt DatasetP2/helper/helper$i.txt output.txt
         echo ""
 done
+
+rm -f input.txt
+rm -f output.txt
